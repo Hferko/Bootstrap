8547125c2757d2f52f99759170d55b4eb18a9070
Hangai Ferko <hferko67@gmail.com>
2021-04-07 18:25:34 +0200

    add base filr structure

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+$(function() {
+    $('#alertButton').click(function(e) {
+        e.preventDefault();
+        $('#alertMe').slideDown();
+    });
+});
+*/
+
+function myFunction() {    
+    document.getElementById('alertMe').style.display='block';
+  }
+
+  
